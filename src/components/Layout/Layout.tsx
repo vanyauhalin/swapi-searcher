3c63a49ec4d25810001b0c73a6dc26679537b399
vanyauhalin <vanyauhalin@gmail.com>
2022-05-20 15:41:19 +0300

    feat: add base layout components

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,43 @@
-import type { PropsWithChildren } from 'react';
-import css from './Layout.module.css';
+import type { ComponentProps, ReactElement } from 'react';
+import { createElement } from 'react';
+import { merge } from 'src/utils';
+import styles from './Layout.module.css';
 
-function Layout(properties: PropsWithChildren<unknown>): JSX.Element {
-  const { children } = properties;
-  return (
-    <main className={css.layout}>
-      {children}
-    </main>
-  );
+function LayoutFooter(properties: ComponentProps<'footer'>): ReactElement {
+  const { className, children } = properties;
+  return createElement('footer', {
+    ...properties,
+    className: merge(styles.LayoutFooter, className),
+  }, children);
+}
+
+function LayoutMain(properties: ComponentProps<'main'>): ReactElement {
+  const { children, className } = properties;
+  return createElement('main', {
+    ...properties,
+    className: merge(styles.LayoutMain, className),
+  }, children);
+}
+
+function LayoutHeader(properties: ComponentProps<'header'>): ReactElement {
+  const { children, className } = properties;
+  return createElement('header', {
+    ...properties,
+    className: merge(styles.LayoutHeader, className),
+  }, children);
+}
+
+function Layout(properties: ComponentProps<'div'>): ReactElement {
+  const { children, className } = properties;
+  return createElement('div', {
+    ...properties,
+    className: merge(styles.Layout, className),
+  }, children);
 }
 
 export {
   Layout,
+  LayoutFooter,
+  LayoutHeader,
+  LayoutMain,
 };
